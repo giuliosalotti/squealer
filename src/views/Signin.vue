1d51e4a53bbff5745a495e4c155cb1ee734a1386
gabritech01 <centonze.gabri@gmail.com>
2023-07-07 22:16:56 +0200

    redirect signin

--- src/views/Signin.vue
+++ src/views/Signin.vue
@@ -2,6 +2,7 @@
 import { defineComponent } from 'vue';
 import Navbar from '../components/Navbar.vue';
 import axios from 'axios';
+import router from '../router';
 
 export default defineComponent({
   name: 'Signin', 
@@ -24,7 +25,7 @@ export default defineComponent({
         quotaW: 3500,
         quotaM: 14000,
       });
-      // Gestisci la risposta dal server qui
+      router.push({ name: 'Login' });
     } catch (error) {
       // Gestisci gli errori qui
     }
@@ -44,16 +45,16 @@ export default defineComponent({
     <form>
         <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input v-model="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <input v-model="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required>
             <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
         </div>
         <div class="mb-3">
             <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input v-model="password" type="password" class="form-control" id="exampleInputPassword1">
+            <input v-model="password" type="password" class="form-control" id="exampleInputPassword1" required>
         </div>
         <div class="mb-3">
             <label for="exampleInputPassword1" class="form-label">Foto profilo</label>
-            <input v-model="foto" type="text" placeholder="https://" class="form-control" id="urlfoto">
+            <input v-model="foto" type="text" placeholder="https://" class="form-control" id="urlfoto" required>
         </div>
         <button @click="signin()" type="submit" class="btn btn-primary">Sign-in</button>
     </form>
